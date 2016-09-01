399b68937c2446171d5902d887ff6c07368ef25c
Ignacio Cruz <igmoweb@gmail.com>
2016-09-01 15:52:56 +0200

    New version

--- cloner.php
+++ cloner.php
@@ -5,7 +5,7 @@ Plugin URI: https://premium.wpmudev.org/project/cloner
 Description: Clone sites in a network installation
 Author: WPMU DEV
 Author URI: http://premium.wpmudev.org/
-Version: 1.7.5
+Version: 1.7.6
 Network: true
 Text Domain: wpmudev-cloner
 Domain Path: lang
@@ -87,7 +87,7 @@ class WPMUDEV_Cloner {
 			define( 'WPMUDEV_COPIER_LANG_DOMAIN', 'wpmudev-cloner' );
 
 		if ( ! defined( 'WPMUDEV_CLONER_VERSION' ) )
-			define( 'WPMUDEV_CLONER_VERSION', '1.7.5' );
+			define( 'WPMUDEV_CLONER_VERSION', '1.7.6' );
 	}
 
 	private function includes() {
